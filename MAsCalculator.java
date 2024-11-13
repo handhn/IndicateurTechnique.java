d79b0ad7cd320a086a5e8ea08b65ee138c484c33
handhn <148367227+handhn@users.noreply.github.com>
2024-11-13 02:31:09 +0100

    Update MAsCalculator.java

--- MAsCalculator.java
+++ MAsCalculator.java
@@ -40,6 +40,9 @@ public class MAsCalculator {
         return ema;
     }
 
+
+    // test 
+    
     public static void main(String[] args) {
         List<Double> prices = List.of(10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0);
         int period = 5;
